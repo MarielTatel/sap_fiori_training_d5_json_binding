902fdc3107f908e4e468c7b4c715bc7c1ea2de50
Mariel Tatel <mariel.tatel@accenture.com>
2026-02-09 11:16:56 +0000

    Added the required features

--- jsonbinding/webapp/controller/JSONBinding.controller.js
+++ jsonbinding/webapp/controller/JSONBinding.controller.js
@@ -1,10 +1,43 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sapips/training/jsonbinding/model/formatter"
+], (Controller, JSONModel, formatter) => {
     "use strict";
 
     return Controller.extend("sapips.training.jsonbinding.controller.JSONBinding", {
-        onInit() {
-        }
+        onInit: function() {
+            var myData = {
+                "Eid": "mariel.tatel",
+                "Enabled": true,
+                "Address": {
+                    "Street": "Aglipay Street",
+                    "City": "Mandaluyong City",
+                    "Zip": "1550",
+                    "Country": "PH"
+                },
+                "SalesAmount": 999999,
+                "CurrencyCode": "Php"
+            };
+
+            var oModelLocal = new JSONModel();
+            oModelLocal.setData(myData);
+            this.getView().setModel(oModelLocal);
+        },
+
+        onSelectProduct: function (oEvent) {
+            // var oList = oEvent.getSource();
+            // var oSelectedItem = oList.getSelectedItem();
+            var oSelectedItem = oEvent.getParameter("listItem");
+            var oBindingContext = oSelectedItem.getBindingContext("ProductsModel");
+            var sPath = oBindingContext.getPath();
+            var oPanel = this.byId("idProdDetForm");
+            oPanel.bindElement({
+                path: sPath,
+                model: "ProductsModel"
+            });
+        },
+
+        formatter: formatter
     });
 });
---
+++ jsonbinding/webapp/model/formatter.js
@@ -0,0 +1,27 @@
+sap.ui.define([
+    "sap/m/library",
+    "sap/ui/model/type/Currency"
+], function (mobileLibrary, Currency) {
+    "use strict";
+
+    return {
+        formatMail: function (sEid) {
+            var i18nModel = new sap.ui.model.resource.ResourceModel({
+                bundleName: "sapips.training.jsonbinding.i18n.i18n"
+            });
+            var oBundle = i18nModel.getResourceBundle();
+
+            return mobileLibrary.URLHelper.normalizeEmail(
+                sEid + oBundle.getText("domain"),
+                oBundle.getText("mailSubject", [sEid]),
+                oBundle.getText("mailBody"));
+        },
+
+        formatStockValue: function (fUnitPrice, iStockLevel, sCurrCode) {
+            var oCurrency = new Currency();
+            return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
+        }
+
+    };
+
+});
